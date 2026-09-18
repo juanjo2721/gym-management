cf6f56fb107da98d718d79c215af09e3110c94c4
Juan Jose Acuna <jjacunariveros@gmail.com>
2026-09-18 12:40:28 -0300

    Configura main como punto de entrada de la aplicacion

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from database import verificar_usuario
-
-usuario = input("Usuario: ")
-contrasena = input("Contraseña: ")
-
-resultado = verificar_usuario(usuario, contrasena)
-
-if resultado:
-    print("Login correcto")
-else:
-    print("Usuario o contraseña incorrectos")
+import login
